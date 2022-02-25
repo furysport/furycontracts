4ef5dfce636e465a9b12f5d837156bbb4bdbf4c9
utkarsh <utkarshvarma.dev@gmail.com>
2022-02-24 21:22:35 -0500

    temp commit

--- testing/gaming.js
+++ testing/gaming.js
@@ -54,12 +54,23 @@ function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+
 const deploy_contract = async function (file, init) {
     const contractId = await storeCode(walletTest1, file,)
-    await sleep(5000)
     const gamingInit = await instantiateContract(walletTest1, contractId, init)
     console.log(`New Contract Init Hash ${gamingInit.txhash}`)
     return gamingInit.logs[0].events[0].attributes[3].value; // Careful with order of argument
+}
+
+
+function convertBinaryToObject(str) {
+    var newBin = str.split(" ");
+    var binCode = [];
+    for (let i = 0; i < newBin.length; i++) {
+        binCode.push(String.fromCharCode(parseInt(newBin[i], 2)));
+    }
+    let jsonString = binCode.join("");
+    return JSON.parse(jsonString)
 }
 
 
@@ -118,7 +129,7 @@ let test_create_and_query_pool = async function (time) {
 
 let test_get_team_count_for_user_in_pool_type = async function (time) {
     console.log("Test Get Team Count In Pool Type")
-    executeContract(walletTest1, gaming_contract_address, {
+    await executeContract(walletTest1, gaming_contract_address, {
         save_team_details: {
             'gamer': "Gamer001",
             'pool_id': "1",
@@ -135,7 +146,7 @@ let test_get_team_count_for_user_in_pool_type = async function (time) {
         }
     })
     sleep(time)
-    executeContract(walletTest1, gaming_contract_address, {
+    await executeContract(walletTest1, gaming_contract_address, {
         save_team_details: {
             'gamer': "Gamer001",
             'pool_id': "1",
@@ -152,7 +163,7 @@ let test_get_team_count_for_user_in_pool_type = async function (time) {
         }
     })
     sleep(time)
-    executeContract(walletTest1, gaming_contract_address, {
+    await executeContract(walletTest1, gaming_contract_address, {
         save_team_details: {
             'gamer': "Gamer001",
             'pool_id': "1",
@@ -169,11 +180,84 @@ let test_get_team_count_for_user_in_pool_type = async function (time) {
         }
     })
 
+    sleep(time)
+    let team_count = await queryContract(gaming_contract_address, {
+        get_team_count_for_user_in_pool_type: {
+            "gamer": "Gamer001",
+            "game_id": "Game001",
+            "pool_type": "oneToOne"
+        }
+    })
+    assert.isTrue(team_count === 3)
+    console.log("Assert Success")
 
+}
+
+let test_game_pool_bid_submit_when_pool_team_in_range = async function (time) {
+    console.log("Test game pool bid submit when pool team in range")
+    let response = executeContract(walletTest1, gaming_contract_address, {
+        set_pool_type_params: {
+            'pool_type': "oneToOne",
+            'pool_fee': "144262",
+            'min_teams_for_pool': 2,
+            'max_teams_for_pool': 10,
+            'max_teams_for_gamer': 2,
+            'wallet_percentages': [
+                {
+                    "wallet_address": "rake_1",
+                    "wallet_name": "rake_1",
+                    "percentage": 1,
+                },
+                {
+                    "wallet_address": "rake_2",
+                    "wallet_name": "rake_2",
+                    "percentage": 2,
+                },
+                {
+                    "wallet_address": "rake_3",
+                    "wallet_name": "rake_3",
+                    "percentage": 3,
+                },
+
+            ]
+        }
+    })
+    let msg = {
+        game_pool_bid_submit_command: {
+            'gamer': "",
+            'pool_type': "",
+            'pool_id': "",
+            'team_id': "",
+        }
+    }
+
+    response = await executeContract(walletTest1, gaming_contract_address, {
+        cw20_receive_msg: {
+            sender: walletTest1.key.accAddress,
+            amount: "10",
+            msg: convertBinaryToObject(JSON.stringify(msg))
+        }
+    })
+    console.log(response)
     console.log("Assert Success")
     sleep(time)
 }
 
+// let test_create_and_query_game = async function (time) {
+//     console.log("Testing Create and Query Game")
+//     gaming_contract_address = await deploy_contract(GamingContractPath, gaming_init)
+//     console.log(`Gaming Address:${gaming_contract_address}`)
+//     let query_resposne = await queryContract(gaming_contract_address, {
+//         game_details: {}
+//     })
+//     assert.isTrue(gaming_init['game_id'] === query_resposne['game_id'])
+//     assert.isTrue(1 === query_resposne['game_status'])
+//     console.log("Assert Success")
+//     sleep(time)
+// }
+
 
 await test_create_and_query_game(sleep_time)
 await test_create_and_query_pool(sleep_time)
+await test_get_team_count_for_user_in_pool_type(sleep_time)
+await test_game_pool_bid_submit_when_pool_team_in_range(sleep_time)
